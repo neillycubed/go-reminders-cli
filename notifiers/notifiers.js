b31f48daffef840aa8ac173b57db8da1b5f691a4
neillycubed <neillybusiness3@gmail.com>
2021-02-17 20:33:53 -0800

    Add notify endpoint functionality

--- notifiers/notifiers.js
+++ notifiers/notifiers.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const notifier = require("node-notifier");
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.get("/health", (req, res) => res.status(200).send())
+app.use(bodyParser.json())
+
+app.get("/health", (req, res) => res.status(200).send());
+app.post("/notify", (req, res) => {
+    notify(req.body, reply => res.send(reply))
+});
 app.listen(port, () => console.log(`Server up and running on port: ${port}`))
+
+const notify = ({title, message}, cb) => {
+    notifier.notify(
+        {
+            title: title || "Unspecified Title",
+            message: message || "Unspecified Message",
+            sound: true,
+            wait: true,
+            reply: true,
+            closeLabel: "Completed?",
+            timeout: 10
+        },
+        (err, response, reply) => {
+            cb(reply)
+        }
+    )
+};
